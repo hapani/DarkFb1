7c9aa7e4dbf8b514a6670a28f02bb983b2937df7
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-08-24 09:41:16 +0700

    Create wibi.py

---
+++ wibi.py
@@ -0,0 +1,5 @@
+_________                           .__  /   _____/ ____   ____   ______ ____ |__|                                           \_____  \_/ __ \ /    \ /  ___// __ \|  |                                           /        \  ___/|   |  \___  \   ___/|  |                                          /_______  /\___  >___|  /____  >\___  >__|                                                  \/     \/     \/     \/     \/   
+             Dark-Fb No login !           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                                    [•] Author : Wibi&R                       [•] Github : github.com/RidwanKechil    [•] Groups : Tricker Facebook Indonesia
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+[01] Crack Using Mobile Phone             [02] Crack Using Email                    [03] Crack From Friendslist & Public [ LOGIN ]
+[00] Exit this program                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                                  [•] ( Choose ) >
